25ed1c506902c0ac9e03117a2a8742a693c0908b
David <bodidavid6@gmail.com>
2021-01-09 02:07:49 +0100

    logout added

--- mini-neptun-client/src/app/app.component.ts
+++ mini-neptun-client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +11,22 @@ import { Title } from '@angular/platform-browser';
 export class AppComponent {
   title = 'Neptun';
 
+  isLoggedIn$: Observable<boolean>;
+
   constructor(
     private ts: Title,  // Az dokumentum címét lehet beállítani és lekérdezni ennek segítségével.
+    protected as: AuthService
   ) {
     ts.setTitle(this.title);
+    this.isLoggedIn$ = as.isLoggedIn();
   }
 
   public setTitle(title: string) {
     this.ts.setTitle(title);  // Dokumentum címének beállítása.
+  }
+
+  logout(): void {
+    this.as.logout();
   }
 
 }
--- mini-neptun-client/src/app/core/services/auth.service.ts
+++ mini-neptun-client/src/app/core/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { baseUrl } from 'src/environments/environment';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from '../interfaces/user';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +17,18 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
+    private router: Router,
   ) { }
+
+  isLogin$ = new BehaviorSubject<boolean>(this.hasToken());
+
+  protected hasToken(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return this.isLogin$.asObservable();
+  }
 
   register(user: User) {
     this.http.post<User>(`${baseUrl}/auth/register`, user, {headers: this.headers}).subscribe(
@@ -30,7 +43,21 @@ export class AuthService {
       data => {
         // console.log(data);
         localStorage.setItem('token', data.token);
+        this.isLogin$.next(true);
+        //this.ns.show('Sikeres bejelentkezés!');
+        this.router.navigate(['/subjects']);
+      },
+      error => {
+        //this.ns.show('HIBA! Bejelentkezés sikertelen!');
+        console.error(error);
       }
     );
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+    this.isLogin$.next(false);
+    //this.ns.show('Sikeres kijelentkezés!');
+    this.router.navigate(['/']);
   }
 }
